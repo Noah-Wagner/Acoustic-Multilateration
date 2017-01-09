433ceb63e7b047231c6f63759e1ba9ed93f0059f
Noah-Wagner <noah@noah-wagner.com>
2017-01-09 13:51:19 -0500

    Added outlines of all project modules

---
+++ Mobile Application/app/src/main/java/com/noah_wagner/acousticmultilateration/MainActivity.java
@@ -0,0 +1,87 @@
+package com.noah_wagner.acousticmultilateration;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateLocation(view);
+            }
+        });
+    }
+
+    public void updateLocation(View view) {
+        Snackbar.make(view, "Location up to date", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(41.076678, -81.513394))
+                            .title("Marker"));
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(new LatLng(41.076678, -81.513394));
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+        googleMap.moveCamera(cameraUpdate);
+        googleMap.animateCamera(zoom);
+        
+    }
+}
--- Multilateration/main.py
+++ Multilateration/main.py
@@ -12,36 +12,51 @@ class MultiSystem:
         self._arrival_times[sensor_id] = time
 
     def perform_multilateration(self):
-        b = numpy.array([0] * (len(self._arrival_times) - 2))
+        self.perform_multilateration_linear()
+
+
+    def perform_multilateration_linear(self):
+        b = [0] * (len(self._arrival_times) - 2)
         a = []
 
+        t_1 = self.SOUND_SPEED * (self._arrival_times[1] - self._arrival_times[0])
         for i in range(2, len(self._arrival_times)):
-            A = ((2 * self._sensor_coords[i][0])/(self.SOUND_SPEED * (self._arrival_times[i] - self._arrival_times[0]))) - \
-                ((2 * self._sensor_coords[1][0]) / (self.SOUND_SPEED * (self._arrival_times[1] - self._arrival_times[0])))
+            t_m = self.SOUND_SPEED * (self._arrival_times[i] - self._arrival_times[0])
 
-            B = ((2 * self._sensor_coords[i][1]) / (self.SOUND_SPEED * (self._arrival_times[i] - self._arrival_times[0]))) - \
-                ((2 * self._sensor_coords[1][1]) / (self.SOUND_SPEED * (self._arrival_times[1] - self._arrival_times[0])))
+            A = ((2 * self._sensor_coords[i][0]) / t_m) - ((2 * self._sensor_coords[1][0]) / t_1)
 
-            C = ((2 * self._sensor_coords[i][2]) / (self.SOUND_SPEED * (self._arrival_times[i] - self._arrival_times[0]))) - \
-                ((2 * self._sensor_coords[1][2]) / (self.SOUND_SPEED * (self._arrival_times[1] - self._arrival_times[0])))
+            B = ((2 * self._sensor_coords[i][1]) / t_m) - ((2 * self._sensor_coords[1][1]) / t_1)
 
-            D = self.SOUND_SPEED*(self._arrival_times[i] - self._arrival_times[1]) - \
-                (self._sensor_coords[i][0]**2 + self._sensor_coords[i][1]**2 + self._sensor_coords[i][2]**2)/(self.SOUND_SPEED * (self._arrival_times[i] - self._arrival_times[0])) + \
-                (self._sensor_coords[1][0] ** 2 + self._sensor_coords[1][1] ** 2 + self._sensor_coords[1][2] ** 2)/(self.SOUND_SPEED * (self._arrival_times[1] - self._arrival_times[0]))
-            a.append(numpy.array([A, B, C, D]))
+            C = ((2 * self._sensor_coords[i][2]) / t_m) - ((2 * self._sensor_coords[1][2]) / t_1)
 
-
-
-        x = numpy.linalg.solve(a, b)
+            D = t_m - t_1 - \
+                ((self._sensor_coords[i][0] ** 2 + self._sensor_coords[i][1] ** 2 + self._sensor_coords[i][2] ** 2)/(t_m)) + \
+                ((self._sensor_coords[1][0] ** 2 + self._sensor_coords[1][1] ** 2 + self._sensor_coords[1][2] ** 2)/(t_1))
+            a.append([A, B, C, D])
+        # a = numpy.array(a)
+        x = numpy.linalg.lstsq(a, b)
         print(x)
+
+
     #
     # class ArrivalCoord:
     #
 
-multiSystem = MultiSystem([(2.0, 3.0, 0.0), (-2.1, 3.1, 0.0), (-1, -3.1, 0.0), (3, -5, 0)])
-multiSystem.addTime(0.0106, 0)
-multiSystem.addTime(0.0110, 1)
-multiSystem.addTime(0.0096, 2)
-multiSystem.addTime(0.0171, 3)
 
-multiSystem.perform_multilateration()
+def perform_test():
+    multiSystem = MultiSystem([(2.0, 3.0, 0.0), (-2.1, 3.1, 0.0), (-1, -3.1, 0.0), (3, -5, 0), (2, 3, 2)])
+    multiSystem.addTime(0.0072, 0)
+    multiSystem.addTime(0.0114, 1)
+    multiSystem.addTime(0.0137, 2)
+    multiSystem.addTime(0.0188, 3)
+    multiSystem.addTime(0.0088, 4)
+
+    multiSystem.perform_multilateration()
+
+
+# Main
+
+perform_test()
+
+# End main
+
