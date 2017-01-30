e2f3b0c806b5e6a691402a1bb8ef504045058f51
Noah-Wagner <noah@noah-wagner.com>
2017-01-30 16:34:13 -0500

    Implemented slope trigger mechanism thing

--- Arduino Capture/waveformID/waveformID.ino
+++ Arduino Capture/waveformID/waveformID.ino
@@ -1,36 +1,25 @@
-const int ANALOG_PIN = 3;     // potentiometer wiper (middle terminal) connected to analog pin 3
-                       // outside leads to ground and +5V
+const int ANALOG_PIN = A3;     // potentiometer wiper (middle terminal) connected to analog pin 3
+                               // outside leads to ground and +5V
 
-const int SAMPLE_LENGTH = 900;
-int val[SAMPLE_LENGTH];           // variable to store the value read
+const int SAMPLE_LENGTH = 200;
+int val[SAMPLE_LENGTH];        // variable to store the value read
+const int THRESHOLD = 30;
+
+int i;
 
 void setup(){ 
-  Serial.begin(9600);                //  setup serial
-
+  Serial.begin(9600);          //  setup serial
+  i = 0;
 }
 
-
-int i = 1; // declare a variable to index the samples
-
-
 void loop(){
-  while(true) {
-    i = i % 900;
-//    Serial.println(i);
-    
+  i = i % SAMPLE_LENGTH;
   val[i] = analogRead(ANALOG_PIN);     // read the input pin
-  delay(10);
-
-//  if (val[i] > val[(i - 1) % SAMPLE_LENGTH] + 500){       //if the intensity is much greater than a previous sample
-//    Serial.println("FLAG2");
-//    Serial.println(val[i]);            //send that value out
+  if (val[i] > ((i == 0) ? val[SAMPLE_LENGTH - 1] : val[i - 1]) + THRESHOLD ){       //if the intensity is much greater than a previous sample;
+    Serial.println(val[i]);            //send that value out
 //    printSomeVal(val, SAMPLE_LENGTH, i);
-//  } 
-//  if (i == 899){
-//    i = -1;
-//  }
-  i++;
-  }
+  } 
+    i++;
 }
 
 //void printSomeVal(int val[], int size, int triggerI) {
@@ -40,12 +29,4 @@ void loop(){
 //  }
 //}
 
- 
-//  val = analogRead(analogPin);
-// 
-//  if (val > preVal + THRESHOLD){       //if the intensity is much greater than a previous sample
-//    Serial.println(val);            //send that value out
-//  }
-//  preVal = val;
- 
 
